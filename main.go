4121d8ede5e1ef15ad2365f25a33b88702431486
Benjamin Petetot <bpetetot@gmail.com>
2015-11-22 18:29:05 +0100

    save arezzo files

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/smtp"
-	"runtime"
-	"strings"
-	"text/template"
+	"os"
+	"os/exec"
 )
 
 // Resp xxx
@@ -67,84 +64,28 @@ func main() {
 			for _, zone := range server.Zones {
 				if (zone.Zone == "fr" || zone.Zone == "westernEurope") && zone.Availability != "unknown" {
 					// send dispo
-					SendEmail("localhost", 25, "", "", []string{""}, "Kimsufi Dispo", SEND)
+					SendEmail("bpetetot@gmail.com", SEND)
 				}
 			}
 			for _, metazone := range server.MetaZones {
 				if (metazone.Zone == "fr" || metazone.Zone == "westernEurope") && metazone.Availability != "unknown" {
 					// send dispo
-					SendEmail("localhost", 25, "", "", []string{""}, "Kimsufi Dispo", SEND)
+					SendEmail("bpetetot@gmail.com", SEND)
 				}
 			}
 		}
 	}
 
-	SendEmail("localhost", 25, "", "", []string{""}, "Kimsufi Dispo", SEND)
-}
-
-func catchPanic(err *error, functionName string) {
-	if r := recover(); r != nil {
-		fmt.Printf("%s : PANIC Defered : %v\n", functionName, r)
-
-		// Capture the stack trace
-		buf := make([]byte, 10000)
-		runtime.Stack(buf, false)
-
-		fmt.Printf("%s : Stack Trace : %s", functionName, string(buf))
-
-		if err != nil {
-			*err = fmt.Errorf("%v", r)
-		}
-	} else if err != nil && *err != nil {
-		fmt.Printf("%s : ERROR : %v\n", functionName, *err)
-
-		// Capture the stack trace
-		buf := make([]byte, 10000)
-		runtime.Stack(buf, false)
-
-		fmt.Printf("%s : Stack Trace : %s", functionName, string(buf))
-	}
+	SendEmail("bpetetot@gmail.com", SEND)
 }
 
 // SendEmail xxx
-func SendEmail(host string, port int, userName string, password string, to []string, subject string, message string) (err error) {
-	defer catchPanic(&err, "SendEmail")
-
-	parameters := struct {
-		From    string
-		To      string
-		Subject string
-		Message string
-	}{
-		userName,
-		strings.Join([]string(to), ","),
-		subject,
-		message,
+func SendEmail(to string, message string) {
+	cmd := "mail"
+	args := []string{"-s", "'dispo'", "-aFrom:Kimsufi Available", to, message}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-
-	buffer := new(bytes.Buffer)
-
-	template := template.Must(template.New("emailTemplate").Parse(emailScript()))
-	template.Execute(buffer, &parameters)
-
-	auth := smtp.PlainAuth("", userName, password, host)
-
-	err = smtp.SendMail(
-		fmt.Sprintf("%s:%d", host, port),
-		auth,
-		userName,
-		to,
-		buffer.Bytes())
-
-	return err
-}
-
-func emailScript() (script string) {
-	return `From: {{.From}}
-To: {{.To}}
-Subject: {{.Subject}}
-MIME-version: 1.0
-Content-Type: text/html; charset="UTF-8"
-
-{{.Message}}`
+	fmt.Println("Successfully halved image in size")
 }
